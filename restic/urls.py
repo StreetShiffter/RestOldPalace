217c0d33e25996242c93195007255aaae6e8bccb
StreetShiffter <agwzdushaz@gmail.com>
2025-11-18 17:11:00 +0700

    fix

--- restic/urls.py
+++ restic/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     path("test/", TestAboutView.as_view(), name="test"),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
